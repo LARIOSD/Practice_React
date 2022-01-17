c6905b5027709e39e33ffed49936a61aaef3e594
lariosd <davidlarios450@gmail.com>
2022-01-17 08:29:24 -0500

    first commit

---
+++ src/PrimeraApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PrimeraApp = () => {
+  const saludo = "Hola saludo";
+  return (
+    <>
+      <h1>{saludo}</h1>
+      <p>Mi primera clase</p>
+    </>
+  );
+};
+
+export default PrimeraApp;
