4218f503f2c5ec5b660f09ddf2b0b1542d0ad206
lariosd <davidlarios450@gmail.com>
2022-04-02 13:24:00 -0500

    *add image logo

--- src/PrimeraApp.jsx
+++ src/PrimeraApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Route } from 'wouter';
+import imgGif from './img/gif.png';
 import ListOfGifs from './components/listOfGits/ListOfGifs';
 
 import './PrimeraApp.css';
@@ -8,6 +9,7 @@ function PrimeraApp() {
   return (
     <div className="App">
       <section className="App-content">
+        <img className="gifLogo" alt="Figgy log" src={imgGif} />
         <Link to="/gif/kimetsunoyaiba">Gifs kimetsunoyaiba</Link>
         <Link to="/gif/anime">Gifs anime</Link>
         <Link to="/gif/rick">Gifs rick</Link>
